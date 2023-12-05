37776418d45c263f249751126b4f6aa2c2cb6981
Artem <vebulogmetra@yandex.ru>
2023-12-05 22:32:34 +0300

    create run.py and implement cli interface :wrench:

---
+++ run.py
@@ -0,0 +1,23 @@
+import importlib.util
+
+import inquirer
+
+from src.core.settings import applications_mapping
+
+questions = [
+    inquirer.List(
+        name="app",
+        message="Выберите приложение",
+        choices=applications_mapping.keys(),
+    )
+]
+
+answers = inquirer.prompt(questions=questions)
+
+selected_application = answers.get("app", None)
+
+
+app_module = importlib.import_module(
+    f"src.{applications_mapping[selected_application]}.service"
+)
+app_module.main()
