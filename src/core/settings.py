a3b492614dd1a06d50496d729f01e1eb62feb837
Artem <vebulogmetra@yandex.ru>
2023-12-05 22:30:48 +0300

    add core app settings

---
+++ src/core/settings.py
@@ -0,0 +1,5 @@
+applications_mapping = {
+    "Youtube as MP3 Downloader": "youtube_mp3_downloader",
+    "Extract code from Youtube video": "code_from_video",
+    "Python library version checker": "lib_update_checker",
+}
